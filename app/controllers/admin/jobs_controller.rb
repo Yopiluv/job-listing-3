34090f2ed10abbd2baaa8ef859536f73eeb77fcd
Yopiluv <wwnchxz@hotmail.com>
2017-07-10 22:59:20 +0800

    database decide the condition of admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
 
   def show
     @job = Job.find(params[:id])
@@ -42,8 +43,15 @@ class Admin::JobsController < ApplicationController
   end
 
   private
-  
+
   def job_params
     params.require(:job).permit(:title, :description)
+  end
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = 'You are not admin!'
+      rediect_to root_path
+    end
   end
 end
